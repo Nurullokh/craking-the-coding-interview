d584fa6f9fbbb1c208c9e2ffff1a8e7ed85b9c52
nurullokh <nnabijonov@students.wiut.uz>
2022-10-01 20:24:46 +0500

    Chapter 1 - problem 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # craking-the-coding-interview
+The solutions to the problems of the book
+called "Cracking the Coding Interview"
---
+++ chapter-1/problem-1.py
@@ -0,0 +1,30 @@
+# Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+# cannot use additional data structures?
+
+# We will assume that string is ASCII (128 characters)
+
+def is_unique(input_str: str) -> bool:
+    if len(input_str) > 128:
+        return False
+    
+    bool_char_set = [False] * 128
+    for char in input_str:
+        char_code_in_ascci = ord(char)
+
+        # return False if char was found already
+        if bool_char_set[char_code_in_ascci]:
+            return False
+        
+        # if not found, change the element in that index to True
+        bool_char_set[char_code_in_ascci] = True
+
+    # if there is no duplicate, return True
+    return True
+
+
+# True
+print(is_unique("abc"))
+
+# False
+print(is_unique("bba"))
+
