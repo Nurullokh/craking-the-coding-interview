3bd98c65ba334e424bf8e6dfd4e44a768d5353d8
nurullokh <nnabijonov@students.wiut.uz>
2022-10-05 20:07:09 +0500

    array definition is added

---
+++ chapter-1/arrays.py
@@ -0,0 +1,6 @@
+# this is a data structure to collect different or the same type elements
+# and it can contain any type of element, there is no restriction.
+arr = [1, "a", 2.0, [1, 2]]
+
+# Accessing an element time complexity - O(1)
+# Inserting and deleting time complexity - O(n) because of replacing other elements
