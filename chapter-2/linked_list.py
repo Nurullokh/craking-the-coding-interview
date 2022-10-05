d10436de0ce6dfcc8cf2348df59977897bc1cb75
nurullokh <nnabijonov@students.wiut.uz>
2022-10-05 20:15:01 +0500

    linked list started

---
+++ chapter-2/linked_list.py
@@ -0,0 +1,17 @@
+# A linked list is a data structure that represents a sequence of nodes.
+# In a singley linked list, each node points to the next node in the linked list. 
+# A boudbly linked list gives each node pointers to both the next and the previous nodes.
+
+
+# Access time complexity is O(n)
+# Inserting and deleting is O(1)
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
