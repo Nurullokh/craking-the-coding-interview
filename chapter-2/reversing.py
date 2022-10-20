7aeb00b542f9a03deacb8e66bf3caddeaf4e6fb8
nurullokh <nnabijonov@students.wiut.uz>
2022-10-20 15:00:39 +0500

    reverse a linked list by recursive approach is added

--- chapter-2/reversing.py
+++ chapter-2/reversing.py
@@ -14,9 +14,34 @@ def reverse_iteratively(head):
     return prev
 # Time complexity is O(n)
 # Space complexity is O(1)
+# # Example
+# item = reverse_iteratively(llist.head)
+# while item:
+#     print(item.val)
+#     item = item.next
 
-item = reverse_iteratively(llist.head)
+
+# recursive approach
+def reverse_recursively(head):
+    def _reverse_recursively(curr, prev):
+        if not curr:
+            return prev
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+
+        return _reverse_recursively(curr, prev)
+    
+    return _reverse_recursively(curr=head, prev=None)
+# time comlexity is O(n)
+# space complexiyt is O(n)
+
+# Example
+item = reverse_recursively(llist.head)
 while item:
     print(item.val)
     item = item.next
+
+
 
