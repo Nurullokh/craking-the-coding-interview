092012d6c6303ee1027c0d0ba71bc443352903da
nurullokh <nnabijonov@students.wiut.uz>
2022-10-26 17:50:50 +0500

    append method of circular linked lists is added

--- chapter-2/circular_linked_list.py
+++ chapter-2/circular_linked_list.py
@@ -12,7 +12,16 @@ class CircularLinkedList:
         pass
     
     def append(self, data):
-        pass
+        if not self.head:
+            self.head = Node(data)
+            self.head.next = self.head
+        else:
+            new_node = Node(data)
+            curr = self.head
+            while curr.next != self.head:
+                curr = curr.next
+            curr.next = new_node
+            new_node.next = self.head
     
     def print_list(self):
         pass
