0ff9c6b0f7f4bd2efb1cb357d58112b320dc7ed8
nurullokh <nnabijonov@students.wiut.uz>
2022-12-01 23:09:49 +0500

    sort linked list with merge sort

--- chapter-2/sort_linked_list.py
+++ chapter-2/sort_linked_list.py
@@ -17,17 +17,17 @@ class Solution:
         return self.merge(l, r)
         
         
-    # def merge(self, l, r):
-    #     if not l or not r:
-    #         return l or r
-    #     dummy = p = ListNode(0)
-    #     while l and r:
-    #         if l.val < r.val:
-    #             p.next = l
-    #             l = l.next
-    #         else:
-    #             p.next = r
-    #             r = r.next
-    #         p = p.next
-    #     p.next = l or r
-    #     return dummy.next
+    def merge(self, l, r):
+        if not l or not r:
+            return l or r
+        dummy = p = ListNode(0)
+        while l and r:
+            if l.val < r.val:
+                p.next = l
+                l = l.next
+            else:
+                p.next = r
+                r = r.next
+            p = p.next
+        p.next = l or r
+        return dummy.next
