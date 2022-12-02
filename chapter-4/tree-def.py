031c390120aa3df3e01581818c804accc97cb09d
nurullokh <nnabijonov@students.wiut.uz>
2022-12-03 02:38:48 +0500

    added helper function

--- chapter-4/tree-def.py
+++ chapter-4/tree-def.py
@@ -31,6 +31,19 @@ class BinaryTree:
             traversal = self.postorder_print(start.right, traversal)
             traversal += str(start.value) + '-'
         return traversal
+    
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        elif traversal_type == "inorder":
+            return self.inorder_print(tree.root, "")
+        elif traversal_type == "postorder":
+            return self.postorder_print(tree.root, "")
+        else:
+            print(f"Traversal type {traversal_type} is not supported")
+            return False
+        
+
 
 tree = BinaryTree(1)
 tree.root.left = Node(2)
@@ -40,4 +53,7 @@ tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
 
+print(tree.print_tree("preorder"))
+print(tree.print_tree("inorder"))
+print(tree.print_tree("postorder"))
 
