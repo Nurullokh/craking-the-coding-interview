a34049dd741f9f2586403c059e8e018958afbe34
nurullokh <nnabijonov@students.wiut.uz>
2023-01-15 21:45:58 +0500

    problem 2 is fixed

--- chapter-3/problem-2.py
+++ chapter-3/problem-2.py
@@ -1,28 +1,53 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self) -> None:
-        self.stack = []
+        self.top = None
         self.minimum = None
-    
-    def append(self, value) -> None:
-        if self.stack:
-            self.minimum = value
+
+    def push(self, data):
+        if self.top is None:
+            self.top = Node(data)
+            self.minimum = data
         else:
-            if self.minimum > value:
-                self.minimum = value
-        self.stack.append(value)
+            temp = Node(data)
+            temp.next = self.top
+            self.top = temp
+            if self.minimum > data:
+                self.minimum = data
     
-    def peek(self) -> int:
-        if self.stack:
-            return self.stack[-1]
-        return "Stack is empty"
+    def peek(self):
+        if self.top is None:
+            return "Stack is empty!"
+        else:
+            return self.top.data
     
-    def pop(self) -> None:
-        if self.stack:
-            self.stack.pop()
-        return "Stack is empty"
+    def pop(self):
+        if self.top is None:
+            return "Stack is empty!"
+        else:
+            temp = self.top.data
+            if self.top.next is None:
+                self.top = None
+            else:
+                self.top = self.top.next
+            return temp
+
+    def is_empty(self):
+        return self.top == None
+    
+    def print_stack(self):
+        temp = self.top
+        while temp:
+            print(temp.data)
+            temp = temp.next    
     
     def min_stack(self):
-        if self.stack:
-            return self.minimum
-        return "Stack is empty"
+        if self.top == None:
+            return "Stack is empty!"
+        return self.minimum
 
